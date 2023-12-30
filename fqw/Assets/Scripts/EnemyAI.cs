1f78345af351c421eaf07979dd59c2d44ff6d54a
icyfirejosh4352 <101503643+icyfirejosh4352@users.noreply.github.com>
2023-12-30 21:25:27 +0530

    sfx

--- fqw/Assets/Scripts/EnemyAI.cs
+++ fqw/Assets/Scripts/EnemyAI.cs
@@ -9,12 +9,15 @@ public class EnemyAI : MonoBehaviour
     public int EnemyHealth = 20;
     public bool hit = false;
     public AxeAttack axe;
+    public AudioSource Slash;
+    public GameObject Death;
     
     // Start is called before the first frame update
     void Start()
     {
         PlayerPos = GameObject.FindGameObjectWithTag("Player").transform;
         enemyAI = gameObject.GetComponent<NavMeshAgent>();
+        Death = GameObject.FindGameObjectWithTag("deathsfx");
     }
 
     // Update is called once per frame
@@ -23,22 +26,24 @@ public class EnemyAI : MonoBehaviour
         enemyAI.SetDestination(PlayerPos.position);
         if(EnemyHealth == 0)
         {
+            Death.GetComponent<AudioSource>().Play();
             Destroy(gameObject);
         }
         
     }
-    void OnTriggerEnter(UnityEngine.Collider other)
+    void OnTriggerEnter(Collider other)
     {
         
         Debug.Log("DMG");
         if (other.gameObject.CompareTag("weapon"))
         {
-
+            Slash.Play();
             EnemyHealth -= other.gameObject.GetComponent<AxeAttack>().AxeDamage;
             hit = true;
             Instantiate(axe.bloodVFX, axe.bloodpos.transform.position, axe.transform.rotation);
         }else if (other.gameObject.CompareTag("Polaroid"))
         {
+            Slash.Play();
             EnemyHealth -= other.gameObject.GetComponent<PolaroidAttack>().PolaroidDmg;
             hit = true;
         }
