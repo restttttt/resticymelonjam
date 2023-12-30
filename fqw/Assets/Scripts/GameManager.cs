b8ed74389913beb4866a1929da0a8f905b24c794
restttttt <darrenfausto1@gmail.com>
2023-12-31 00:50:20 +0800

    try it

--- fqw/Assets/Scripts/GameManager.cs
+++ fqw/Assets/Scripts/GameManager.cs
@@ -5,25 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public int i = 0;
+
     public int j = 0;
-    public GameObject[] TalismanPrefabs;
+ 
     public GameObject[] Charms;
     public GameObject[] CharmSpawnPoints;
-    public GameObject[] TalismanSpawnPoints;
+
 
     // Update is called once per frame
     void Start()
     {
-        TalismanSpawnPoints = GameObject.FindGameObjectsWithTag("Talisman Spawnpoint");
         CharmSpawnPoints = GameObject.FindGameObjectsWithTag("Item Spawnpoint");
 
-        foreach(GameObject obj in TalismanPrefabs)
-        {
-            Debug.Log("Inst 1  " + i);
-            Instantiate(obj, TalismanSpawnPoints[i].transform.position, Quaternion.identity);
-            i++;
-        }
+        
         foreach (GameObject obj in CharmSpawnPoints)
         {
             Debug.Log("Inst 2  " + j);
@@ -34,9 +28,5 @@ public class GameManager : MonoBehaviour
             Instantiate(Charms[j], obj.gameObject.transform.position, Quaternion.identity);
             j++;
         }
-    }
-    public void Respawn()
-    {
-        SceneManager.LoadScene(0);
     }
 }
---
+++ fqw/Assets/Scripts/talismanget.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class talismanget : MonoBehaviour
+{
+    public float score = 0f;
+    public Camera cam;
+    public float range = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+{
+    if (hit.transform.CompareTag("Talisman"))
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(hit.transform.gameObject);
+            score += 1f;
+        }
+    }
+}
+    }
+}
